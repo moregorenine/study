a90d70d9873087744c4be50fcca7cee99c90f3c0
moregorenine <moregorenine@gmail.com>
2018-10-30 01:30:14 +0900

    기본 spring boot 파일 초기화

---
+++ spring-boot-quick-start/src/main/java/com/w4springrain/springbootquickstart/SpringBootQuickStartApplication.java
@@ -0,0 +1,12 @@
+package com.w4springrain.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootQuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootQuickStartApplication.class, args);
+	}
+}
