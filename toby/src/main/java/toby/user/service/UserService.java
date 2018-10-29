8e73a21c9d1b14b0c3b360b3d0cbd2a571ba1900
moregorenine <moregorenine@gmail.com>
2018-10-30 01:09:38 +0900

    repo 초기화

--- toby/src/main/java/toby/user/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package toby.user.service;
-
-import toby.user.domain.User;
-
-public interface UserService {
-	public void add(User user);
-	public void upgradeLevels();
-}
--- toby/src/test/java/learningtest/jdk/Hello.java
+++
@@ -1,7 +0,0 @@
-package learningtest.jdk;
-
-public interface Hello {
-	String sayHello(String name);
-	String sayHi(String name);
-	String sayThankYou(String name);
-}
