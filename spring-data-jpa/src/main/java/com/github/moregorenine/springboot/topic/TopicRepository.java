54940c38eb4f3760abc63d00030a966dcca0cf8f
moregorenine <moregorenine@gmail.com>
2018-11-15 23:48:46 +0900

    Repository @Service anotation remove

--- spring-data-jpa/src/main/java/com/github/moregorenine/springboot/topic/TopicRepository.java
+++ spring-data-jpa/src/main/java/com/github/moregorenine/springboot/topic/TopicRepository.java
@@ -1,8 +1,6 @@
 package com.github.moregorenine.springboot.topic;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface TopicRepository extends CrudRepository<Topic, String> {
 }
