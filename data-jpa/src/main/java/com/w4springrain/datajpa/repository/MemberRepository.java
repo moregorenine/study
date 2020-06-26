f8f8d2d57a1647070261ca5f105633b29c5871d1
moregorenine <moregorenine@gmail.com>
2020-06-27 02:27:52 +0900

    inflean data-jpa 메소드 이름으로 쿼리 생성

--- data-jpa/src/main/java/com/w4springrain/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/com/w4springrain/datajpa/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.w4springrain.datajpa.repository;
 import com.w4springrain.datajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
 }
