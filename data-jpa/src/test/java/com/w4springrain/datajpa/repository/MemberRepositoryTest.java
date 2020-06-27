d061a63a49903887becf4342e75ceff2d7cb0154
moregorenine <moregorenine@gmail.com>
2020-06-27 10:31:48 +0900

    inflean data-jpa @Query, 레포지토리 메소드에 쿼리 정의하기

--- data-jpa/src/test/java/com/w4springrain/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/com/w4springrain/datajpa/repository/MemberRepositoryTest.java
@@ -68,4 +68,17 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void findUser() {
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+        List<Member> result =
+                memberRepository.findUser("AAA", 10);
+        assertThat(result.get(0).getUsername()).isEqualTo("AAA");
+        assertThat(result.get(0).getAge()).isEqualTo(10);
+        assertThat(result.size()).isEqualTo(1);
+    }
 }
