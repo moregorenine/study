e9bb6924689db49ab242b757a1d1c5ea109a0aca
moregorenine <moregorenine@gmail.com>
2020-06-29 00:28:46 +0900

    inflean data-jpa 순수 JPA 페이징과 정렬

--- data-jpa/src/test/java/com/w4springrain/datajpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/com/w4springrain/datajpa/repository/MemberJpaRepositoryTest.java
@@ -68,4 +68,38 @@ class MemberJpaRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void paging() {
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 10));
+        memberJpaRepository.save(new Member("member3", 10));
+        memberJpaRepository.save(new Member("member4", 10));
+        memberJpaRepository.save(new Member("member5", 10));
+        memberJpaRepository.save(new Member("member6", 10));
+        memberJpaRepository.save(new Member("member7", 10));
+        memberJpaRepository.save(new Member("member8", 10));
+        memberJpaRepository.save(new Member("member9", 10));
+        memberJpaRepository.save(new Member("member10", 10));
+        memberJpaRepository.save(new Member("member11", 10));
+        memberJpaRepository.save(new Member("member12", 10));
+        memberJpaRepository.save(new Member("member13", 10));
+        memberJpaRepository.save(new Member("member14", 10));
+        memberJpaRepository.save(new Member("member15", 10));
+        memberJpaRepository.save(new Member("member16", 10));
+        memberJpaRepository.save(new Member("member17", 10));
+        memberJpaRepository.save(new Member("member18", 10));
+        memberJpaRepository.save(new Member("member19", 10));
+        memberJpaRepository.save(new Member("member20", 10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(20);
+    }
 }
